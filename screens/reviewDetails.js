4234fc738b3b590e127cc6f9f46ada7dfa9f3987
SinkLineP <popovartem505@gmail.com>
2023-03-03 08:46:23 +0300

    create initial structure

---
+++ screens/reviewDetails.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+
+export default function reviewDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>reviewDetails Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24
+  }
+})
