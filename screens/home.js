0df1aca7ad3647682788fac44e335f2a88a525c2
SinkLineP <popovartem505@gmail.com>
2023-03-06 10:48:58 +0300

    create init state

--- screens/home.js
+++ screens/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, Button} from "react-native";
 import { globalStyles } from "../styles/global";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,19 +8,18 @@ import {StatusBar} from "expo-status-bar";
 const HomeStack = createNativeStackNavigator();
 
 export default function Home({ navigation }) {
+  const [reviews, setReviews] = useState([
+    {title: "Фантастические твари и где они обитают", rating: 7.6, body: "2016 год" },
+    {title: "Фантастические твари: Преступления Грин-де-Вальда", rating: 6.6, body: "2018 год" },
+    {title: "Фантастические твари: Тайны Дамблдора", rating: 6, body: "2022 год" }
+  ])
 
-  const pressHandler = (route) => {
-    navigation.navigate(route)
-  }
 
   return (
     <>
       <View style={globalStyles.containerNoFlex}>
         <Text style={globalStyles.titleText}>Home Screen</Text>
-        <Button
-          title={"show details"}
-          onPress={() => pressHandler("ReviewDetails")}
-        />
+
         <StatusBar style="auto" />
       </View>
 
