77352e7f400786252219e9c01dab6830dee11f33
SinkLineP <popovartem505@gmail.com>
2023-03-09 10:00:18 +0300

    created show rating

--- components/Rating/Rating.js
+++ components/Rating/Rating.js
@@ -1,27 +1,43 @@
 import React, {useState} from "react";
 import {View, StyleSheet, Text} from "react-native";
 import {arrayToString, isFloat} from "../../options/Mathes";
+import fillStarIcon from "./Icons/fillStar.svg";
+import unfilledStarIcon from "./Icons/unfilledStar.svg";
+import halfFilledStarIcon from "./Icons/halfFilledStar.svg";
 
 
 export default function Rating({currentRating, maxRating}) {
   const curRating = Math.floor(currentRating);
   const [star, setStar] = useState([]);
+  const remainStars = [];
 
   const showRating = (star, curRating, setStar) => {
+    const remainingStars = Math.floor(maxRating - currentRating);
+
+
     if (star.length !== curRating) {
       for (let i = 0; i < curRating; i++) {
         setStar([...star, "*"])
       }
+
     }
+
+    if (remainingStars.length !== 0) {
+      for (let i = 0; i < remainingStars; i++) {
+        remainStars.push("0");
+      }
+    }
+
 
     if (isFloat(currentRating)) {
       const numberAfterComa = Number(currentRating.toString().split(".")[1]);
 
+
       if (numberAfterComa > 0 && numberAfterComa !== 0) {
-        return <Text>{arrayToString(star)+"/"}</Text>;
+        return <Text>{arrayToString(star) + "/" + arrayToString(remainStars)}</Text>;// выводит оценку с плавающей запятой
       }
     } else {
-      return <Text>{arrayToString(star)}</Text>;
+      return <Text>{arrayToString(star) + arrayToString(remainStars)}</Text>;// выводит оценку целым числом
     }
   }
 
