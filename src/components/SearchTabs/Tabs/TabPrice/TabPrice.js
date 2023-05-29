b5ebca423c9fb9b5c5898cb8cd26025e116df38e
SinkLineP <popovartem505@gmail.com>
2023-05-29 17:49:06 +0300

    fixed show modal count rooms

--- src/components/SearchTabs/Tabs/TabPrice/TabPrice.js
+++ src/components/SearchTabs/Tabs/TabPrice/TabPrice.js
@@ -7,17 +7,13 @@ import {PortalProvider} from "@gorhom/portal";
 import Slider from "@react-native-community/slider";
 
 
-export default function TabPrice() {
+export default function TabPrice({ modalRef }) {
   const [minPrice, setMinPrice] = useState("");
   const [maxPrice, setMaxPrice] = useState("");
   const [multiLevel, setMultiLevel] = useState(false);
   const [kitchenLivingRoom, setKitchenLivingRoom] = useState(false);
-  const [isStudio, setIsStudio] = useState(false);
   const symbolCurrency = "₽";
-  const modalCountRoomsRef = useRef(null);
 
-  const [range, setRange] = useState(1);
-  const [sliding, setSliding] = useState("Inactive");
 
 
   return (
@@ -33,7 +29,7 @@ export default function TabPrice() {
         <View style={stylesTabPrice.containerCountRoom}>
           <Text style={stylesTabPrice.title}>Количество комнат</Text>
           <Text style={stylesTabPrice.btnCountRooms} onPress={() => {
-            modalCountRoomsRef.current?.open()
+            modalRef.current?.open()
           }}>Выберите..</Text>
         </View>
         <View style={stylesTabPrice.containerSwitch}>
@@ -61,43 +57,6 @@ export default function TabPrice() {
           />
         </View>
       </ContainerTab>
-      <PortalProvider>
-        <BottomModalWindow modalRef={modalCountRoomsRef}>
-          <View style={stylesTabPrice.containerModal}>
-            <View style={stylesTabPrice.containerCountRoom}>
-              <Text style={stylesTabPrice.title}>Студия</Text>
-              <Switch
-                value={isStudio}
-                onValueChange={() => {
-                  setRange(1);
-                  setIsStudio(!isStudio);
-                }}
-                disabled={false}
-                renderActiveText={false}
-                renderInActiveText={false} barHeight={20} circleSize={20}
-              />
-            </View>
-          </View>
-          {isStudio !== true ? (
-            <View style={stylesTabPrice.containerSlider}>
-              <Text style={stylesTabPrice.title}>Количество комнат: {range === 10 ? range + "+" : range}</Text>
-
-              <Slider
-                style={{width: "100%", marginTop: 15}}
-                minimumValue={1}
-                maximumValue={10}
-                minimumTrackTintColor={"tomato"}
-                maximumTrackTintColor={"#000"}
-                thumbTintColor={"tomato"}
-                value={range}
-                onSlidingComplete={value => {
-                  setRange(Math.floor(value))
-                }}
-              />
-            </View>
-          ) : ("")}
-        </BottomModalWindow>
-      </PortalProvider>
     </>
   )
 }
@@ -126,12 +85,6 @@ const stylesTabPrice = StyleSheet.create({
     justifyContent: "space-between",
     paddingVertical: 5
   },
-  containerCountRoom: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 5,
-    alignItems: "center"
-  },
   btnCountRooms: {
     backgroundColor: "tomato",
     color: "white",
@@ -140,12 +93,10 @@ const stylesTabPrice = StyleSheet.create({
     borderRadius: 10,
     fontWeight: "bold"
   },
-  containerSlider: {
-    paddingBottom: 20,
-    paddingHorizontal: 20,
-  },
-  containerModal: {
-    paddingHorizontal: 20,
-    paddingBottom: 10
+  containerCountRoom: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 5,
+    alignItems: "center"
   }
 })
